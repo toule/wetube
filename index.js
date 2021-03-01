771a945b8392345f0c9aba3ad059db9a5d1ffa00
ray.h.li <lhs6395@gmail.com>
2021-03-02 00:36:58 +0900

    express

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("Hi");
+//const express = require('express');
+import "core-js";
+import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+/*function handleHome(req, res) {
+    res.send("Hello form home");
+}*/
+const handleHome = (req, res) => res.send("Hello from home!!");
+
+/*function handleProfile(req, res) {
+    res.send("You are on my profile");
+}*/
+
+const betweenHome = (req, res, next) => {
+    console.log("Between My Home!");
+    next();
+}
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+/*function handleListening() {
+    console.log(`listening on http://localhost:${PORT}`);
+}*/
+const handleListening = (req, res) => console.log(`listening on http://localhost:${PORT}`);
+
+app.use(betweenHome);
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(logger("combined"));
+
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
